b53554990cb20faa85ef375c41f279dba76421db
imaesp <127232789+imaesp@users.noreply.github.com>
2024-07-22 14:57:00 -0400

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
       <div className="wrapper">
         <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>ima.esp</motion.span> 
         <div className='social'>
-          <a href="#"><img src="src\assets\facebook.png" alt="" /></a>
-          <a href="#"><img src="src\assets\instagram.png" alt="" /></a>
-          <a href="#"><img src="src\assets\github.svg" alt="" /></a>
+          <a href="#"><img src="assets\facebook.png" alt="" /></a>
+          <a href="#"><img src="assets\instagram.png" alt="" /></a>
+          <a href="#"><img src="assets\github.svg" alt="" /></a>
         </div>
       </div>
     </div>
