84cdfefcde1961f9c129bf9f40baeda2e12013a7
imaesp <127232789+imaesp@users.noreply.github.com>
2024-08-08 10:02:08 -0400

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ function About() {
         </motion.div>
       </div>
       <div className="imageContainer">
-        <img src="..\headshot.jpg" alt="" />
+        <img src="./headshot.jpg" alt="" />
       </div>
     </div>
   );
