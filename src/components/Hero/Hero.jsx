81c7be369c49832d3688abe5ac00020c467919ef
imaesp <127232789+imaesp@users.noreply.github.com>
2025-01-24 14:35:02 -0400

    Updated color-scheme

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -40,25 +40,39 @@ const textVariants = {
 };
 
 function Hero() {
-  return (
-    <div className='hero'>
-        <div className='wrapper'>
-            <motion.div className='textContainer' variants={textVariants} initial="initial" animate="animate">
-                <motion.h2 variants={textVariants}>IMANOL ESPINAL</motion.h2>
-                <motion.h1 variants={textVariants}>Full Stack Web Developer</motion.h1>
-                <motion.div className='buttons' variants={textVariants}>
-                    <motion.button variants={textVariants}>See My Work</motion.button>
-                    <motion.button variants={textVariants}>Contact Me</motion.button>
-                    <motion.a href='https://drive.google.com/file/d/1ERMMSxIBTvqrIP32YgjJhCyTSNNPuFLI/view' download class='btn btn--download' className='resume-btn'>My resume</motion.a>
+
+    const scrollToProjects = () => {
+        document.getElementById('Projects')?.scrollIntoView({
+            behavior: 'smooth', 
+            block: 'start',
+        });
+    };
+    const scrollToContact = () => {
+        document.getElementById('Contact')?.scrollIntoView({
+            behavior: 'smooth', 
+            block: 'start',
+        });
+    };
+
+    return (
+        <div className='hero'>
+            <div className='wrapper'>
+                <motion.div className='textContainer' variants={textVariants} initial="initial" animate="animate">
+                    <motion.h2 variants={textVariants}>IMANOL ESPINAL</motion.h2>
+                    <motion.h1 variants={textVariants}>Full Stack Web Developer</motion.h1>
+                    <motion.div className='buttons' variants={textVariants}>
+                        <motion.button variants={textVariants} onClick={scrollToProjects}>See My Work</motion.button>
+                        <motion.button variants={textVariants} onClick={scrollToContact}>Contact Me</motion.button>
+                        <motion.a href='https://drive.google.com/file/d/1ERMMSxIBTvqrIP32YgjJhCyTSNNPuFLI/view' download class='btn btn--download' className='resume-btn'>My resume</motion.a>
+                    </motion.div>
+                    <motion.img variants={textVariants} animate="scrollButton" src="./scroll.png" alt="" />
                 </motion.div>
-                <motion.img variants={textVariants} animate="scrollButton" src="./scroll.png" alt="" />
+            </div>
+            <motion.div className='slidingTextContainer' variants={sliderVariants} initial="initial" animate="animate">
+                Java Python CSS JavaScript HTML SQL TypeScript
             </motion.div>
         </div>
-        <motion.div className='slidingTextContainer' variants={sliderVariants} initial="initial" animate="animate">
-            Java Python CSS JavaScript HTML SQL TypeScript
-        </motion.div>
-    </div>
-  )
+    )
 }
 
 export default Hero
